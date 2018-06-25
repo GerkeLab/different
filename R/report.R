49d4e57ed6645fa3d9f74b062fbc6b30fa8b3add
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-06-25 14:41:54 -0400

    Move diffing to inside report() function and update replace `value.x|y` with df name

--- R/report.R
+++ R/report.R
@@ -9,11 +9,13 @@ report <- function(reference, comparison, df_names = NULL, outfile = NULL, ...) 
   outdir <- dirname(outfile)
   outfile <- basename(outfile)
 
+  df_diff <- tidy_diff(reference, comparison, df_names = df_names, ...)
+
   rpt <- rmarkdown::render(
     input = system.file("report.Rmd", package = "different"),
     output_file = outfile,
     output_dir = outdir,
-    params = list(df_ref = reference, df_cmp = comparison, df_names = df_names)
+    params = list(df_diff = df_diff)
   )
   if (rstudioapi::isAvailable()) {
     rstudioapi::viewer(rpt)
